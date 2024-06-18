be68899ea7899e6994ba6160970adf39e1d3d045
gavinarori <arorigavin@gmail.com>
2024-06-18 09:36:34 +0300

    added:pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
 const nextConfig = {
     serverComponentsExternalPackages: ['@prisma/client', 'bcrypt']
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
