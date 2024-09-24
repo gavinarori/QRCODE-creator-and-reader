8f230024cb6ac1e8664e96a3c9dca8ec5bbfee41
gavinarori <arorigavin@gmail.com>
2024-09-24 11:08:20 +0300

    added map to the bicycle-lock

--- src/app/components/map/Map.tsx
+++ src/app/components/map/Map.tsx
@@ -1,11 +1,11 @@
 "use client"
-
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { LatLngExpression, LatLngTuple } from 'leaflet';
-
+import { LatLngExpression, LatLngTuple, divIcon } from 'leaflet';
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
+import { useEffect } from "react";
+import "./map.css"
 
 interface MapProps {
     posix: LatLngExpression | LatLngTuple,
@@ -16,8 +16,12 @@ const defaults = {
     zoom: 19,
 }
 
-const Map = (Map: MapProps) => {
-    const { zoom = defaults.zoom, posix } = Map
+const Map = ({ zoom = defaults.zoom, posix }: MapProps) => {
+
+    const liveLocationIcon = divIcon({
+        className: 'live-location-icon',
+        html: '<div class="pulse"></div>',
+    });
 
     return (
         <MapContainer
@@ -30,11 +34,11 @@ const Map = (Map: MapProps) => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={posix} draggable={false}>
-                <Popup>Hey ! I study here</Popup>
+            <Marker position={posix} icon={liveLocationIcon} draggable={false}>
+                <Popup>Hey! Your bicycle is here</Popup>
             </Marker>
         </MapContainer>
     )
 }
 
-export default Map
+export default Map;
