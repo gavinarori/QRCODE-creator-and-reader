696ffa854c3dcc87f13323f5cd714a2b524a7330
gavinarori <arorigavin@gmail.com>
2024-10-18 13:27:46 +0300

    added map component

--- src/app/components/map/page.tsx
+++ src/app/components/map/page.tsx
@@ -1,20 +1,41 @@
+"use client"
 import dynamic from "next/dynamic";
-import { useMemo } from "react";
+import { useMemo, useState, useEffect } from "react";
 
-export default async function Page() {
+export default function Page() {
     const Map = useMemo(() => dynamic(
         () => import('./Map'),
         {
             loading: () => <p>A map is loading</p>,
             ssr: false
         }
-    ), [])
+    ), []);
+
+    const [currentLocation, setCurrentLocation] = useState<[number, number] | null>(null);
+
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const { latitude, longitude } = position.coords;
+                    setCurrentLocation([latitude, longitude]);
+                },
+                (error) => {
+                    console.error("Error fetching location:", error);
+                    setCurrentLocation([-1.1973489, 36.9301873]); 
+                }
+            );
+        } else {
+            console.error("Geolocation is not supported by this browser.");
+        }
+    }, []);
 
     return (
         <>
             <div className="bg-white-700 mx-auto my-5 w-[98%] h-[480px]">
-                <Map posix={[4.79029, -75.69003]} />
+                {currentLocation ? <Map posix={currentLocation} /> : <p>Loading current location...</p>}
             </div>
         </>
-    )
+    );
 }
